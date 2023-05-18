1aafe333023447ade516e7c68fbbd46aea2951e2
PlayDay <18056374+playday3008@users.noreply.github.com>
2023-05-18 17:49:34 +0200

    Yippee

---
+++ android/app/src/main/kotlin/dev/playday/chatgram/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.playday.chatgram
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:chatgram/utils/constants.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:chatgram/pages/splash_page.dart';
+
+Future<void> main() async {
+  // Ensure that binding is initialized before calling Supabase and dotenv
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Load dotenv file
+  await dotenv.load(fileName: "dotenv");
+
+  // Initialize Supabase
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+  );
+
+  // Run app
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ChatGram', // Title of the app
+      theme: appTheme, // Theme defined in utils/constants.dart
+      home: const SplashPage(), // First page to load
+    );
+  }
+}
